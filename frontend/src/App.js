51300743e72c31a185aa7893130254b617376dde
ttytu <quixote1103@gmail.com>
2024-03-07 17:53:28 +0900

    user handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
 import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
 
 import Nav from './components/Nav';
 import Footer from './components/Footer';
@@ -10,14 +11,32 @@ import Footer from './components/Footer';
 
 function App() {
 	const location = useLocation();
+	const [userId, setUserId] = useState(null);
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const response = await fetch(`http://localhost:8000/v1/user/${userId}`);
+				const data = await response.json();
+				setUser(data);
+				console.log('user:', data);
+			} catch (error) {
+				console.error('Error fetching user:', error);
+			}
+		};
+
+		fetchUser();
+	}, [userId]);
 
 	return (
 		<div className="App text-slate-50 bg-slate-950 min-w-fit">
-			<Nav />
+			<Nav user={userId} setUser={setUserId} />
 
-			<div className='max-w-screen-lg mx-auto pt-24'>
+			<div className='mx-auto'>
 				<Routes location={location}>
-					<Route key={location.pathname} path='/' element={<Home />} />
+					<Route key={location.pathname} path='/' element={<Home user={user} />} />
+					<Route key={location.pathname} path='/movie/:id' element={<MovieDetails movieId={location.pathname.split('/')[2]} />} />
 				</Routes>
 			</div>
 
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 
-const Nav = () => {
+const Nav = ({ user, setUser }) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [isloggedIn, setIsLoggedIn] = useState(false);
+	const location = useLocation();
+
+	const loginhandler = (userId) => {
+		if (isloggedIn) {
+			setUser(null);
+			setIsLoggedIn(false);
+		} else if (userId > 0) {
+			setUser(userId);
+			setIsLoggedIn(true);
+		}
+	};
+
 	return (
 		<nav className="fixed bg-opacity-30 p-4 bg-slate-950 backdrop-blur-xl z-50 w-full border-b-[1px] border-b-slate-800">
-			<div className="max-w-screen-lg mx-auto flex">
-				<Link to="/" className="">Home</Link>
+			<div className="max-w-screen-lg mx-auto flex justify-between">
+				<Link to="/" className="flex gap-1 hover:text-slate-300">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+					</svg>
+					<span className="font-bold">
+						DBRS
+					</span>
+				</Link>
+
+				<div className={`flex flex-col overflow-hidden`}>
+					<button
+						className={`group transition ease-in-out self-end`}
+						onClick={() => setIsOpen(!isOpen)}
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`group-hover:text-slate-300 w-6 h-6`}>
+							<path
+								key={isOpen ? "open" : "closed"}
+								strokeLinecap="round" strokeLinejoin="round"
+								d={isOpen ?
+									"M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" :
+									"M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"}
+							/>
+						</svg>
+					</button>
+
+					<div className={`${isOpen ? "h-[40px] pt-2" : "h-0"} gap-[1px] transition-all flex`}>
+						<input
+							id='selectUser'
+							type="text"
+							placeholder="Select User"
+							className={`bg-slate-500 text-slate-50 rounded-s-full transition-all px-4 w-32 ${isloggedIn ? 'pointer-events-none opacity-50' : ''}`}
+							disabled={isloggedIn}
+						/>
+						<button
+							onClick={() => loginhandler(document.querySelector('#selectUser').value)}
+							className={`bg-slate-500 text-slate-50 px-4 rounded-e-full transition-all ${isloggedIn ? '' : ''}`}
+						>
+							{isloggedIn ? "Logout" : "Login"}
+						</button>
+					</div>
+				</div>
 			</div>
-		</nav>
+		</nav >
 	);
 };
 
